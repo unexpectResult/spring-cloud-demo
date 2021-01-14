ad1b270af2daa6066a35198bc8b57f9c431e4d28
unexpectResult <1270901509@qq.com>
2021-01-14 09:30:10 +0800

    first-auth

---
+++ spring-cloud-demo-eureka/src/main/java/com/cloud/eureka/EurekaRunner.java
@@ -0,0 +1,16 @@
+package com.cloud.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaRunner {
+
+    public static void main(String[] args){
+        SpringApplication.run(EurekaRunner.class,args);
+        System.out.println("启动Eureka");
+    }
+
+}
